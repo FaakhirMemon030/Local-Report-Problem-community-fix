d68e710d9be60693bd14b7b737fd4da5d0117d0a
FaakhirMemon030 <alexwatson031105@gmail.com>
2026-03-01 05:14:45 +0500

    feat: Add `StorageService` for uploading problem images to Cloudinary.

--- local_report_problem_community_fix/lib/services/storage_service.dart
+++ local_report_problem_community_fix/lib/services/storage_service.dart
@@ -5,18 +5,17 @@ import 'package:image_picker/image_picker.dart';
 class StorageService {
   static const String cloudName = "faakhirmemon"; 
   static const String uploadPreset = "lrpcfa";
-  static const String apiKey = "STAGu0W7v0DPq8GMRnUZUUEFlJs";
 
   /// Uploads image to Cloudinary using REST API (No CORS issues on Web)
   Future<String?> uploadProblemImageXFile(XFile file, String problemId) async {
     try {
       final bytes = await file.readAsBytes();
       
+      // Multi-part request for Unsigned Upload
       final url = Uri.parse('https://api.cloudinary.com/v1_1/$cloudName/image/upload');
       
       final request = http.MultipartRequest('POST', url)
         ..fields['upload_preset'] = uploadPreset
-        ..fields['api_key'] = apiKey
         ..files.add(http.MultipartFile.fromBytes(
           'file',
           bytes,
